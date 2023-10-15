11597d75b438fa46869f9d9c30bd51ba9e06c617
Lucas Burson <ljdelight@gmail.com>
2023-10-14 21:39:41 -0500

    Add a sql comment describing the purpose of challenges.widget_layout

--- conf/evolutions/default/93.sql
+++ conf/evolutions/default/93.sql
@@ -1,6 +1,9 @@
 -- --- !Ups
 ALTER TABLE IF EXISTS challenges
-ADD COLUMN widget_layout jsonb NOT NULL DEFAULT '{}'::jsonb;
+    ADD COLUMN widget_layout jsonb NOT NULL DEFAULT '{}'::jsonb;
+
+COMMENT ON COLUMN challenges.widget_layout IS
+    'The challenges.widget_layout is a json body that the GUI uses as a "suggested layout" when displaying the challenge to editors.';
 
 -- --- !Downs
 ALTER TABLE IF EXISTS challenges
