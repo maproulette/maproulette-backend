c799fd3fc56e59df441790fc9d094da8b867b2ca
CollinBeczak <ctbeczak08@gmail.com>
2024-08-10 19:06:13 -0500

    remove unneeded constraint

--- conf/evolutions/default/95.sql
+++ conf/evolutions/default/95.sql
@@ -9,15 +9,7 @@ ALTER TABLE IF EXISTS users
 ALTER COLUMN avatar_url SET DEFAULT '/assets/images/user_no_image.png',
 ALTER COLUMN avatar_url SET NOT NULL;
 
--- Add a CHECK constraint to ensure avatar_url is a string (TEXT type)
-ALTER TABLE IF EXISTS users
-ADD CONSTRAINT check_avatar_url_is_string CHECK (pg_typeof(avatar_url) = 'text');
-
 -- --- !Downs
--- Remove the CHECK constraint
-ALTER TABLE IF EXISTS users
-DROP CONSTRAINT IF EXISTS check_avatar_url_is_string;
-
 -- Revert the avatar_url column to allow null values and remove the default value
 ALTER TABLE IF EXISTS users
 ALTER COLUMN avatar_url DROP NOT NULL,
