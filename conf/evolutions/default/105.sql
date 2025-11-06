1da5cda89d3950f00952daf329c25a03d76ba450
Collin Beczak <ctbeczak08@gmail.com>
2025-11-06 15:51:00 -0600

    remove incorrect down

--- conf/evolutions/default/105.sql
+++ conf/evolutions/default/105.sql
@@ -9,8 +9,4 @@ CREATE UNIQUE INDEX idx_challenges_parent_id_name ON challenges (parent_id, lowe
 WHERE (deleted = false OR deleted IS NULL);;
 
 # --- !Downs
--- Restore original unique constraint on (parent_id, name) without deleted filter
 DROP INDEX IF EXISTS idx_challenges_parent_id_name;;
-
-SELECT create_index_if_not_exists('challenges', 'parent_id_name', '(parent_id, lower(name))', true);;
-
