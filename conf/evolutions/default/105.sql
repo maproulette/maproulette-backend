93780f3aac769bccd33a4151ea7c8ea6302938bc
Collin Beczak <ctbeczak08@gmail.com>
2026-05-29 12:29:50 -0300

    Revert formatting/IF NOT EXISTS edits to already-applied migrations 105 and 106
    Restore migrations 105 and 106 to their original content. Previously-applied
    migrations should not be modified: Play tracks each migration's checksum and
    will refuse to apply new ones if history does not match.

--- conf/evolutions/default/105.sql
+++ conf/evolutions/default/105.sql
@@ -5,7 +5,7 @@
 -- This allows challenge names to be reused after deletion
 DROP INDEX IF EXISTS idx_challenges_parent_id_name;;
 
-CREATE UNIQUE INDEX idx_challenges_parent_id_name ON challenges (parent_id, lower(name))
+CREATE UNIQUE INDEX idx_challenges_parent_id_name ON challenges (parent_id, lower(name)) 
 WHERE (deleted = false OR deleted IS NULL);;
 
 # --- !Downs
